c7204ca3412eb10ab4eddd324c08932a362af022
Jerome <ducampdorgasjerome@gmail.com>
2021-07-26 17:17:21 +0200

    modif temporaire schema produit

--- models/produit.js
+++ models/produit.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const produitSchema = mongoose.Schema({
-    id: { type: Number, required: true },
     title: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
     colors: { type: String, required: false },
-    imageUrl: { type: String, required: true },
-    Stock: { type: Number, required: true },
-    price: { type: Number, required: true },
+    imageUrl: { type: String, required: false },
+    Stock: { type: Number, required: false },
+    price: { type: Number, required: false },
 });
 
 module.exports = mongoose.model('Produit', produitSchema);
